eea64d137036554010eefb13ffe233cf518d12e6
Bill Chirrick <billchirrick@gmail.com>
2021-09-02 18:14:07 -0700

    git stuff

--- day_2/practice_2.py
+++ day_2/practice_2.py
@@ -9,3 +9,16 @@
 # Take an user's input
 # Assign a new variable for each step
 # at the end, use an if statement to see if the final is always 5!
+
+
+a = input("Pick a number from 1 - 9 ") 
+a=int(a)
+b = a
+a *= 2
+a += 10
+a /= 2
+a = a - b
+if a==5:
+    print("final is always 5!")
+elif a!=5:
+    print("final is NOT always 5!")
